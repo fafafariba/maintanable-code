c1c612f5da717c4cabb4e951f9b4e9d5a49d4a69
Massah <fmassah@cj.com>
2017-09-29 11:36:09 -0700

    Change artifactId and update README

--- src/main/java/com/seefaribacode/onboarding/EntryServlet.java
+++ src/main/java/com/seefaribacode/onboarding/EntryServlet.java
@@ -1,4 +1,16 @@
 package com.seefaribacode.onboarding;
 
-public class EntryServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EntryServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
 }
