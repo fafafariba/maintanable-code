f5a7049f0e6e8d8afda735a45d2c5da4b305d85f
Massah <fmassah@cj.com>
2017-10-02 17:25:43 -0700

    Setup rest of tests

---
+++ src/test/java/com/seefaribacode/onboarding/ResponseHandlerTest.java
@@ -0,0 +1,19 @@
+package com.seefaribacode.onboarding;
+
+import org.junit.Test;
+
+public class ResponseHandlerTest {
+
+    @Test
+    public void shouldWriteHelloResponse(){
+
+    }
+
+    public void shouldWriteAddResponse(){
+
+    }
+
+    public void shouldWriteNotFoundResponse(){
+
+    }
+}
---
+++ src/test/java/com/seefaribacode/onboarding/RouteHandlerTest.java
@@ -0,0 +1,12 @@
+package com.seefaribacode.onboarding;
+
+import org.junit.Test;
+
+public class RouteHandlerTest {
+
+    @Test
+    public void shouldDoSomething(){
+
+    }
+
+}
