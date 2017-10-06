d90b7cf9a6025cb7c9e51327848d08541e1d41ea
Massah <fmassah@cj.com>
2017-10-05 17:46:03 -0700

    Initial commit

---
+++ src/main/java/com/seefaribacode/onboarding/EntryServlet.java
@@ -0,0 +1,6 @@
+package com.seefaribacode.onboarding;
+
+import javax.servlet.http.HttpServlet;
+
+public class EntryServlet extends HttpServlet {
+}
---
+++ src/main/java/com/seefaribacode/onboarding/RequestHandler.java
@@ -0,0 +1,15 @@
+package com.seefaribacode.onboarding;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RequestHandler {
+
+    public ResponseHandler dispatch(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
+
+class ResponseHandler{
+
+}
